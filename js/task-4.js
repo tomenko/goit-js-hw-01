e19c82fc8a3e032d45d81949694ac8ed5902e83f
tomenko <tomenko.sl@gmail.com>
2020-11-30 20:15:24 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -12,18 +12,18 @@ else { message = `Вы купили ${totalPrice / pricePerDroid} дроида-(
 
 
 /* --------------------2-й вариант-------------------- */
-/* if (totalPrice) {
+if (totalPrice) {
     if (totalPrice === null) { totalPrice = "Отменено пользователем!" }
     if (totalPrice = Number(totalPrice) * pricePerDroid) { totalPrice = totalPrice; console.log("Сумма покупки") }
     if (credits < totalPrice) { message = "Недостаточно средств на счету!" } else { message = `Вы купили ${totalPrice / pricePerDroid} дроида-(ов), на счету осталось ${credits - totalPrice} кредитов.` }
 
-} */
+}
 
 /* --------------------3-й вариант-------------------- */
-if (totalPrice === null) { console.log("Отменено пользователем!") } else if (totalPrice = Number(totalPrice) * pricePerDroid) {
+/* if (totalPrice === null) { console.log("Отменено пользователем!") } else if (totalPrice = Number(totalPrice) * pricePerDroid) {
     totalPrice; console.log("Сумма покупки")
     if (credits < totalPrice) { message = "Недостаточно средств на счету!" } else { message = `Вы купили ${totalPrice / pricePerDroid} дроида-(ов), на счету осталось ${credits - totalPrice} кредитов.` }
-}
+} */
 
 
 
